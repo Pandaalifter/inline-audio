b9c48d5bdc455adc3d8b4cd74daf9e31d2003fbe
Pandaa <96748487+Pandaalifter@users.noreply.github.com>
2023-04-18 18:00:58 -0400

    Saucy

--- src/audio-app.js
+++ src/audio-app.js
@@ -28,7 +28,10 @@ class AudioApp extends LitElement {
 
   render() {
     return html`
-          The King of Burgers made a decree. The decree came in the form of a song. Not just any song, but a legendary song that bellowed to the world. This song was of <inline-audio audio-file="${new URL('../assets/whopper.mp3', import.meta.url).href}">whoppers</inline-audio> and toppers.
+        <div>
+            Richard Stallman once sang the <inline-audio>Open Source song.</inline-audio>
+        </div>
+            The King of Burgers made a decree. The decree came in the form of a song. Not just any song, but a legendary song that bellowed to the world. This song was of <inline-audio audio-file="${new URL('../assets/whopper.mp3', import.meta.url).href}">whoppers</inline-audio> and toppers.
     `;
   }
 }
--- src/inline-audio.js
+++ src/inline-audio.js
@@ -38,7 +38,7 @@ class InlineAudio extends LitElement {
 
   constructor() {
     super();
-    this.audioFile = '../assets/software-song.mp3';
+    this.audioFile = new URL('../assets/software-song.mp3', import.meta.url).href;
     this.playerIcon = "av:play-arrow";
     this.isPlaying = false;
   }
@@ -56,7 +56,7 @@ class InlineAudio extends LitElement {
   }
 
   handleClickEvent(){
-    if(this.shadowRoot.querySelector('audio').paused == true){
+    if(this.shadowRoot.querySelector('audio').paused){
       this.shadowRoot.querySelector('.player').play();
       this.isPlaying = true;
       this.playerIcon = "av:pause";
